ba9f71d46e1846182c1da902047d95c124926519
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-08-01 10:08:03 +0300

    add mx error to http error translation

--- utils.go
+++ utils.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"mime"
+	"net"
 	"net/http"
 	"strings"
 
 	"github.com/mdigger/log"
 	"github.com/mdigger/rest"
+	"gopkg.in/mdigger/mx.v2"
 )
 
 // getToken возвращает авторизационный токен из запроса
@@ -42,4 +44,30 @@ func jsonBind(r *http.Request, v interface{}) *rest.Error {
 		return rest.NewError(http.StatusUnsupportedMediaType,
 			"Unsupported mediatype")
 	}
+}
+
+// httpError подменяет ошибки, возвращаемые сервером MX на ошибки HTTP.
+func httpError(err error) *rest.Error {
+	// в зависимости от типа ошибки возвращаем разное описание
+	switch err := err.(type) {
+	case *mx.LoginError: // ошибка авторизации пользователя
+		return rest.NewError(http.StatusForbidden, err.Error())
+	case *mx.TimeoutError: // ошибка ожидания ответа
+		return rest.NewError(http.StatusGatewayTimeout, err.Error())
+	case *mx.CSTAError: // ошибка от сервера MX
+		return rest.NewError(http.StatusBadRequest, err.Error())
+	}
+	// сервер MX не отвечает
+	if err, ok := err.(net.Error); ok {
+		if err.Timeout() {
+			log.Warn("mx response timeout", err)
+			return rest.NewError(http.StatusGatewayTimeout,
+				"MX response timeout")
+		}
+		log.Error("mx not availible", err)
+		return rest.NewError(http.StatusBadGateway, "MX not availible")
+	}
+	// ошибка не предусмотрена обработчиком
+	log.Error("mx error", err)
+	return rest.NewError(http.StatusInternalServerError, "MX error")
 }
