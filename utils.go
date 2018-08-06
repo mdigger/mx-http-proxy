2362a38378ed0da8f8995753a50ea141a57f9cb0
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-08-06 19:37:02 +0300

    remove unused getToken function

--- utils.go
+++ utils.go
@@ -12,17 +12,6 @@ import (
 	"github.com/mdigger/rest"
 )
 
-// getToken возвращает авторизационный токен из запроса
-func getToken(r *http.Request) string {
-	const authMethodName = "Bearer "
-	// запрашивает токен авторизации из заголовка или параметра запроса
-	var token = r.Header.Get("Authorization")
-	if strings.HasPrefix(token, authMethodName) {
-		return strings.TrimPrefix(token, authMethodName)
-	}
-	return r.URL.Query().Get("access_token")
-}
-
 // jsonBind разбирает запрос в формате JSON и сериализует его в указанный объект.
 func jsonBind(r *http.Request, v interface{}) *rest.Error {
 	mediatype, params, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
